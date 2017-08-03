fae2c07d5197ce77396545ca1b8c19a37c536649
eekrupin <eekrupin@gmail.com>
2017-08-03 23:39:19 +0300

    HW4. Alfa 1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,42 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM meals WHERE id=:id AND user_id=:user_id"),
+        @NamedQuery(name = Meal.DELETE_ALL, query = "DELETE FROM meals WHERE user_id=:user_id"),
+        @NamedQuery(name = Meal.GET, query = "SELECT * FROM meals WHERE id = :id AND user_id = :user_id"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT * FROM meals WHERE user_id=:user_id ORDER BY date_time DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT * FROM meals WHERE user_id=:user_id  AND date_time BETWEEN :startDate AND :endDate ORDER BY date_time DESC")
+})
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = "user_id, dateTime", name = "meals_unique_user_datetime_idx")})
 public class Meal extends BaseEntity {
+
+    public static final String DELETE = "meal.delete";
+    public static final String DELETE_ALL = "meal.deleteAll";
+    public static final String GET = "meal.get";
+    public static final String GET_ALL = "meal.getAll";
+    public static final String GET_BETWEEN = "meal.getBetween";
+
+    @Column(name = "dateTime", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotEmpty
     private String description;
 
+    @Column(name = "calories", nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Meal() {
