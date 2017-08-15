59c2364eef3b9e15c0554f435abcb07ff015c0cd
eekrupin <eekrupin@gmail.com>
2017-08-15 20:07:05 +0300

    HW5. Completed after review. Correct cumulative output.

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -56,6 +56,7 @@ public abstract class AbstractServiceTest {
                 "\n---------------------------------\n" +
                 results +
                 "---------------------------------\n");
+        results.setLength(0);
     }
 
 }
